b21b91df6a19711efd63c0212e7c7fa0799388e8
abdullahasson <bh532997@gmail.com>
2024-06-25 06:20:03 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
